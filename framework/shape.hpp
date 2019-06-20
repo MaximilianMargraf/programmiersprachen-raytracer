bbb947eba6aa3a343182e0b2dad482b076c0fec8
MaximilianMargraf <m.margraf.m@googlemail.com>
2019-06-20 15:51:37 +0200

    Task 5.1 finished

---
+++ framework/shape.hpp
@@ -0,0 +1,15 @@
+#ifndef SHAPE_HPP
+#define SHAPE_HPP
+
+#include <iostream>
+
+class Shape{
+public:
+	virtual float area();
+	virtual float volume();
+};
+
+
+
+
+#endif // SHAPE_HPP
--- source/tests.cpp
+++ source/tests.cpp
@@ -1,6 +1,8 @@
 #define CATCH_CONFIG_RUNNER
 #include <catch.hpp>
 
+#include "shape.hpp"
+
 int main(int argc, char *argv[])
 {
   return Catch::Session().run(argc, argv);
