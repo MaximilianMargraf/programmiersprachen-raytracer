b706b6f2d3b4a5edef0fd9dc1f45fec632eebfb6
GottaGoGitHub <ericgrunitz@gmail.com>
2019-07-31 23:54:36 +0200

    Changed Sphere intersect

--- framework/sphere.cpp
+++ framework/sphere.cpp
@@ -52,43 +52,19 @@ std::ostream& Sphere::print(std::ostream& os) const{
 }
 
 HitPoint Sphere::intersect(Ray const& ray){
-	HitPoint hp;
+    HitPoint hitpoint;
+    Ray norm;
+    norm.direction = glm::normalize(ray.direction);
+    float distance = 0.0f;
 
-	// vector between origin and center
-	glm::vec3 origin_center = center - ray.origin;
+    hitpoint.intersected = glm::intersectRaySphere(ray.origin, norm.direction, center, radius * radius, distance);
+    hitpoint.name = name_;
+    //hitpoint.color = color_;
+    hitpoint.distance = distance;
 
-	// projecting this vector on the direction vector
-	float skalar = glm::dot(origin_center, ray.direction);
-	glm::vec3 least_distance = skalar * ray.direction;
-
-	// distance to point with least distance from center of sphere
-	float distance = glm::distance(least_distance, center);
-	//float distance = sqrt(glm::dot(origin_center, origin_center) -
-	//						glm::dot(least_distance, least_distance));
-
-	//if this distance is > radius, the ray misses the sphere
-	if(distance > radius){
-		hp.intersected = false;
-	}
-	else{
-		hp.intersected = true;
-	}
-
-	// vector from center to point with least distance
-	glm::vec3 center_least = center - least_distance;
-
-	// intersection point
-	float length = sqrt(pow(radius, 2) - glm::dot(center_least, center_least));
-	glm::vec3 intersection_point = least_distance - (length * ray.direction);
-
-	hp.intersection_point = intersection_point;
-
-	hp.name = name_;
-	hp.direction = ray.direction;
-	hp.distance = sqrt(glm::dot(hp.intersection_point, intersection_point));
-
-	return hp;
+    return hitpoint;
 }
+
 
 std::ostream& operator<<(std::ostream& os, Sphere const& s){
 	s.print(os);
